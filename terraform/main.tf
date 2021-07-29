f4a7404ed193644d942b489858c23643aa7bd22f
Margarita Krutikova <margaretkru@gmail.com>
2021-07-29 15:56:09 +0200

    Add backend terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ variable "region" {
 
 # providers
 terraform {
-   required_providers {
+  required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "~>2.0"
@@ -16,7 +16,6 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  subscription_id = var.azure_subscription_id
 }
 
 resource "azurerm_resource_group" "local" {
