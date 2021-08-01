d04935e1ebe0e74b8fd9e75e93666848bc8ba7d2
Margarita Krutikova <margaretkru@gmail.com>
2021-08-01 19:57:35 +0200

    Experiment with workflow per branch

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,18 @@ variable "region" {
   default = "Norway East" 
 }
 
+variable "app_name" {
+  default = "terraform-env"
+}
+
+# locals
+
+locals {
+  rg_name = "${terraform.workspace}_${var.app_name}-rg"
+  app_service_plan_name = "${terraform.workspace}_${var.app_name}-service-plan"
+  app_name = "${terraform.workspace}_${var.app_name}-app"
+}
+
 # providers
 terraform {
   required_providers {
@@ -17,14 +29,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "local" {
-  name = "terraform-env-per-branch-rg"
+  name = local.rg_name
   location = var.region
 }
 
 # app service
 
 resource "azurerm_app_service_plan" "local" {
-  name = "terraform-env-per-branch-appserviceplan"
+  name = local.app_service_plan_name
   location = azurerm_resource_group.local.location
   resource_group_name = azurerm_resource_group.local.name
   kind = "Linux"
@@ -36,7 +48,7 @@ resource "azurerm_app_service_plan" "local" {
 }
 
 resource "azurerm_app_service" "local" {
-  name = "terraform-env-per-branch-appservice"
+  name = local.app_name
   location = azurerm_resource_group.local.location
   resource_group_name = azurerm_resource_group.local.name
   app_service_plan_id = azurerm_app_service_plan.local.id
